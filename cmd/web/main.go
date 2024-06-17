728fdb753fb8cf716c9c9dcbe6a4fb54066a2b03
doooooooomiki <dominic.kirste@gmail.com>
2024-06-17 08:30:09 +0200

    :lipstick:

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,6 +13,15 @@ func main() {
 	// as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
 
+	// Create a file server which serves files out of the "./ui/static" directory.
+	// Note that the path given to the http.Dir function is relatice to the directory root.
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+
+	// Use the mux.Handle() functioin to register the file server as the handler for
+	// all URL paths that start with "/static/". For matching paths, we strip the
+	// "/static" prefix before the request reaches the file server
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
 	// Prefix the route patterns with the required HTTP method
 	// (for now, we will restrict all three routes to acting on GET requests).
 	mux.HandleFunc("GET /{$}", home)
